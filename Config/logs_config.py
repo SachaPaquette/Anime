1c3c399c2d0f10e5b244e7854403d28f63c9e8a2
Sacha <le1829079@crc-lennox.qc.ca>
2023-11-19 12:37:52 -0500

    Removed unnecessary lines in logs_config.py

--- Config/logs_config.py
+++ Config/logs_config.py
@@ -1,19 +1,13 @@
 import logging
-import json
+
 def setup_logging(logger_name, log_file):
     logger = logging.getLogger(logger_name) # Create a logger
     logger.setLevel(logging.INFO) # Set the log level to INFO of the logger
-    
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s') # Format the log messages
-    
     file_handler = logging.FileHandler(log_file) # Log to a file
     file_handler.setLevel(logging.INFO) # Set the log level to INFO of the file handler
     file_handler.setFormatter(formatter) # Set the formatter for the file handler
-    
     logger.addHandler(file_handler) # Add the file handler to the logger
 
     return logger
 
-def process_browser_log_entry(entry):
-    response = json.loads(entry['message'])['message']
-    return response
