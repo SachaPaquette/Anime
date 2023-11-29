44c434f0ce4545e4442ad1180f2007d2edc8e9b7
Sacha Paquette <le1829079@crc-lennox.qc.ca>
2023-11-29 10:58:47 -0500

    Added documentation in VideoPlayer.py and removed unused functions

--- AnimeWatcher/VideoPlayer.py
+++ AnimeWatcher/VideoPlayer.py
@@ -1,47 +1,50 @@
 from python_mpv_jsonipc import MPV
 
+
 class VideoPlayer:
     _instance = None
 
     def __new__(cls):
         if cls._instance is None:
+            # Create a new instance if one doesn't exist
             cls._instance = super(VideoPlayer, cls).__new__(cls)
+            # Initialize the observer_id to None
             cls._instance.observer_id = None
-            cls._instance.mpv = None  # Initialize mpv to None initially
+            cls._instance.mpv = None  # Initialize the mpv instance to None
             return cls._instance
         return cls._instance
+
     def initialize_player(self):
         try:
             if self.mpv is not None:
                 self.terminate_player()  # Terminate the existing player instance
-
+            # Create a new MPV instance
             self.mpv = MPV()
+            # Make the video player fullscreen by default
             self.mpv.fullscreen = True
         except Exception as e:
             raise e
 
     def play_video(self, url):
         try:
+            # Initialize the MPV instance
             self.initialize_player()
+            # Try to play the video
             self.mpv.play(url)
-            
+
         except Exception as e:
             # Recreate the MPV instance and try again if the socket is closed
             if "socket is closed" in str(e):
+                # Terminate the MPV instance
                 self.mpv = None
+                # Reinitialize the MPV instance
                 self.initialize_player()
+                # Retry playing the video
                 self.play_video(url)
-                
+
             else:
                 raise e
 
-
-
-    def quit_command(self):
-        try:
-            self.mpv.command("quit")
-        except Exception as e:
-            raise e
     def terminate(self):
         """
         Terminate the MPV instance.
@@ -51,6 +54,7 @@ class VideoPlayer:
                 self.mpv.command("quit")  # Corrected method call
         except Exception as e:
             raise e
+
     def terminate_player(self):
         try:
             if self.mpv is not None:
@@ -69,6 +73,7 @@ class VideoPlayer:
 
     def is_open(self):
         return self.mpv is not None
+
     def check_if_socket_is_open(self):
         """
         Check if the socket is open.
