a740b1cb63688b1f5df7f2d3a76ecb61f04553ab
Sacha Paquette <le1829079@crc-lennox.qc.ca>
2023-11-30 09:07:35 -0500

    Added some refactoring in UrlOperations

--- AnimeWatcher/UrlOperations.py
+++ AnimeWatcher/UrlOperations.py
@@ -230,10 +230,10 @@ class UrlInteractions:
         """
         try:
             return (
-                        AES.new(key, self.mode, iv=iv)
-                        .decrypt(base64.b64decode(data))
-                        .strip(b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10")
-                    )
+                AES.new(key, self.mode, iv=iv)
+                .decrypt(base64.b64decode(data))
+                .strip(b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10")
+            )
         except Exception as e:
             raise Exception(f"Error during AES decryption of data: {e}")
 
