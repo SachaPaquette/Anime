af4e349c0871c1b8f93a6add5cb2963a38e1748a
Sacha <le1829079@crc-lennox.qc.ca>
2023-12-19 12:10:38 -0500

    Added documentation of scripts' use

--- animefetch.py
+++ animefetch.py
@@ -1,3 +1,4 @@
+# Script to fetch anime titles from the Gogoanime website and store them in the database
 from AnimeFetch.FetchOperations import AnimeFetch
 from Config.logs_config import setup_logging
 from Config.config import AnimeFetcherConfig
@@ -8,10 +9,10 @@ logger = setup_logging('anime_fetch', AnimeFetcherConfig.ANIME_FETCH_LOG_PATH)
 
 if __name__ == "__main__":
     try:
-        web_interactions = WebInteractions() # Instantiate the WebInteractions class
-        anime_interactions = AnimeInteractions(web_interactions) 
-        anime_fetch = AnimeFetch(web_interactions, anime_interactions) 
-        anime_fetch.main() # Call the main method
+        web_interactions = WebInteractions()  # Instantiate the WebInteractions class
+        anime_interactions = AnimeInteractions(web_interactions)
+        anime_fetch = AnimeFetch(web_interactions, anime_interactions)
+        anime_fetch.main()  # Call the main method
     except Exception as e:
         logger.error(f"Unexpected exception: {e}")
         raise  # Re-raise the exception to stop further execution
