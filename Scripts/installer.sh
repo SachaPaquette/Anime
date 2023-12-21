4c8d7f84795d9752734d9ba6c4be4f461ba889da
sach <le1829079@crc-lennox.qc.ca>
2023-12-20 23:13:48 -0500

    pull

--- Scripts/installer.sh
+++ Scripts/installer.sh
@@ -5,7 +5,7 @@ echo "Downloading and installing Google Chrome..."
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -f  # Install dependencies
-rm google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb # Remove the 
 
 # Download and install MPV
 echo "Downloading and installing MPV..."
