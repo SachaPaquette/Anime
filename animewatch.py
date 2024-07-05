d6b87c834d0e9bbe567fcc861b98cbc657efe9d8
sach <sachapaquette12@gmail.com>
2024-07-05 10:45:54 -0400

    Refactored main function, removed lines that werent clean

--- animewatch.py
+++ animewatch.py
@@ -2,22 +2,17 @@
 from AnimeWatcher.WatchOperations import Main
 from Config.logs_config import setup_logging
 from Config.config import AnimeWatcherConfig
-
 # Set up the logger
-logger = setup_logging(AnimeWatcherConfig.ANIME_WATCH_LOG_FILENAME,
-                       AnimeWatcherConfig.ANIME_WATCH_LOG_PATH)
+logger = setup_logging(AnimeWatcherConfig.ANIME_WATCH_LOG_FILENAME,AnimeWatcherConfig.ANIME_WATCH_LOG_PATH)
 
 def main():
     try:
-        main = Main()  # Instantiate the class
-        main.main()  # Call the main method
-    except Exception as e:
-        logger.error(f"Unexpected exception: {e}")
-        raise  # Re-raise the exception to stop further execution
+        Main().main()
     except KeyboardInterrupt:
-        print("\nExiting...")
-        exit()
-
+        exit(0)
+    except Exception as e:
+        logger.error(f"Unexpected exception in animewatch.py: {e}")
+        raise   
 if __name__ == "__main__":
     main()
         
